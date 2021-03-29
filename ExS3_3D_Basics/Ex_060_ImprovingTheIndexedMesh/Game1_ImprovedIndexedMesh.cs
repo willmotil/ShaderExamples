d341fd970665272b3063812318439f44fed6756b
willmotil <willmotil@users.noreply.github.com>
2021-03-29 00:00:08 -0400

    ...
    ...

--- ExS3_3D_Basics/Ex_060_ImprovingTheIndexedMesh/Game1_ImprovedIndexedMesh.cs
+++ ExS3_3D_Basics/Ex_060_ImprovingTheIndexedMesh/Game1_ImprovedIndexedMesh.cs
@@ -24,10 +24,10 @@ namespace ShaderExamples
 
         float[] heightMap = new float[]
         {
-            0.1f, 0.1f, 0.0f, 0.0f, 0.0f,
-            0.0f, 0.0f, 0.3f, 0.0f, 0.0f,
-            0.0f, 0.0f, 0.2f, 0.0f, 0.0f,
-            0.0f, 0.0f, 0.1f, 0.0f, 0.0f
+            0.1f, 0.2f, 0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.8f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.5f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.1f, 0.9f, 0.0f
         };
 
         Matrix view;
@@ -43,6 +43,8 @@ namespace ShaderExamples
         Vector3 quadUpVector = Vector3.Up;
         Vector3 quadForwardVector = Vector3.Forward;
         float quadRotation = 0;
+
+        bool displayWireframe = false;
 
         public Game1_ImprovedIndexedMesh()
         {
@@ -89,11 +91,12 @@ namespace ShaderExamples
             SimpleDrawingWithMatrixClassEffect.Projection = projection;
 
             PrimitiveIndexedMesh.showOutput = true;
-            
-            //mesh = new PrimitiveIndexedMesh(4,4, 300f, true, false, true);
-            
-            mesh = new PrimitiveIndexedMesh(heightMap, 5, 300f, true, false, true);
-            
+
+            //mesh = new PrimitiveIndexedMesh(4,4, new Vector3(300f, 250, 0f ));
+
+            //mesh = new PrimitiveIndexedMesh(heightMap, 5, new Vector3( 300f, 250, 100f ));
+
+            mesh = new PrimitiveIndexedMesh(texture, new Vector3(300f, 250, 5f ));
         }
 
         protected override void UnloadContent()
@@ -155,6 +158,9 @@ namespace ShaderExamples
             if (Keys.F1.IsKeyPressedWithDelay(gameTime))
                 InitialView();
 
+            if (Keys.F2.IsKeyPressedWithDelay(gameTime))
+                displayWireframe = !displayWireframe;
+
             base.Update(gameTime);
         }
 
@@ -180,6 +186,7 @@ namespace ShaderExamples
         {
             GraphicsDevice.Clear(Color.Moccasin);
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
             GraphicsDevice.SamplerStates[0] = SamplerState.LinearClamp;
             GraphicsDevice.RasterizerState = rasterizerState_CULLNONE_SOLID;
 
@@ -187,19 +194,28 @@ namespace ShaderExamples
             SimpleDrawingWithMatrixClassEffect.Projection = projection;
             SimpleDrawingWithMatrixClassEffect.World = Matrix.Identity;
 
-            // draw regularly
-            GraphicsDevice.RasterizerState = rasterizerState_CULLNONE_SOLID;
-            SimpleDrawingWithMatrixClassEffect.SpriteTexture = texture;
-            mesh.DrawPrimitive(GraphicsDevice, SimpleDrawingWithMatrixClassEffect.effect);
+            DrawMesh();
 
-            // draw the wireframe
-            GraphicsDevice.RasterizerState = rasterizerState_CULLNONE_WIREFRAME;
-            SimpleDrawingWithMatrixClassEffect.SpriteTexture = dotTexture;
-            mesh.DrawPrimitive(GraphicsDevice, SimpleDrawingWithMatrixClassEffect.effect);
+            if(displayWireframe)
+                DrawWireFrameMesh();
 
             DrawSpriteBatches(gameTime);
 
             base.Draw(gameTime);
+        }
+
+        public void DrawMesh()
+        {
+            GraphicsDevice.RasterizerState = rasterizerState_CULLNONE_SOLID;
+            SimpleDrawingWithMatrixClassEffect.SpriteTexture = texture;
+            mesh.DrawPrimitive(GraphicsDevice, SimpleDrawingWithMatrixClassEffect.effect);
+        }
+
+        public void DrawWireFrameMesh()
+        {
+            GraphicsDevice.RasterizerState = rasterizerState_CULLNONE_WIREFRAME;
+            SimpleDrawingWithMatrixClassEffect.SpriteTexture = dotTexture;
+            mesh.DrawPrimitive(GraphicsDevice, SimpleDrawingWithMatrixClassEffect.effect);
         }
 
         public void DrawSpriteBatches(GameTime gameTime)
@@ -210,9 +226,10 @@ namespace ShaderExamples
                 $" \n The camera exists as a world matrix that holds a position and orientation." +
                 $" \n The keys WASD change the forward view direction (which is the major take away here). ZC allows for spin." +
                 $" \n The Arrows move the camera translation as strafing motion. " +
+                $" \n  The F2 key will turn a wireframe on or off." +
                 $" \n  " +
-                $" \n In this example we draw a extremely simple index mesh and map a texture onto it." +
-                $" \n We then draw the mesh with a rasterizerstate we have created to turn on wireframe with a dot texture." +
+                $" \n In this example we improve the previous mesh we all the mesh to take a height map." +
+                $" \n The map can be in the form of a array or of a texture we also create normals for the mesh." +
                 $" \n " +
                 $" \n " +
                 $" \n { cameraWorld.DisplayMatrix("cameraWorld") }" +
@@ -282,40 +299,90 @@ namespace ShaderExamples
         }
 
 
+
+        //_____________________________________________________
+        //_____________________________________________________
+        //
+
         public class PrimitiveIndexedMesh
         {
             public static bool showOutput = false;
+            public static int AveragingOption { get; set; } = AVERAGING_OPTION_USE_NONALPHACONSISTANTLY;
+
+            public const int AVERAGING_OPTION_USE_NONALPHACONSISTANTLY = 3;
+            public const int AVERAGING_OPTION_USE_HIGHEST = 2;
+            public const int AVERAGING_OPTION_USE_AVERAGE = 1;
+            public const int AVERAGING_OPTION_USE_RED = 0;
 
             public VertexPositionNormalTexture[] vertices;
             public int[] indices;
 
+            Color[] heightColorArray;
+
             public PrimitiveIndexedMesh()
             {
-                CreatePrimitiveMesh(2, 2, 1f, false, true, true, 0);
+                int w = 2;
+                int h = 2;
+                heightColorArray = new Color[w * h];
+                for (int i = 0; i < w * h; i++)
+                {
+                    heightColorArray[i].R = 0;
+                    heightColorArray[i].A = 0;
+                }
+                CreatePrimitiveMesh(heightColorArray, 2, Vector3.Zero);
+                heightColorArray = new Color[0];
+            }
+            public PrimitiveIndexedMesh(int subdivisionWidth, int subdividsionHeight, Vector3 scale)
+            {
+                heightColorArray = new Color[subdivisionWidth * subdividsionHeight];
+                for (int i = 0; i < subdivisionWidth * subdividsionHeight; i++)
+                {
+                    heightColorArray[i].R = 0;
+                    heightColorArray[i].A = 0;
+                }
+                CreatePrimitiveMesh(heightColorArray, subdivisionWidth, scale);
+                heightColorArray = new Color[0];
             }
 
             public PrimitiveIndexedMesh(float[] heightArray, int strideWidth)
             {
-                CreatePrimitiveMesh(heightArray, strideWidth, 1f, false, true, true, 0);
+                heightColorArray = new Color[heightArray.Length];
+                for (int i = 0; i < heightArray.Length; i++)
+                {
+                    heightColorArray[i].R = GetAvgHeightFromFloatAsByte(heightArray[i]);
+                    heightColorArray[i].A = GetAvgHeightFromFloatAsByte(heightArray[i]);
+                }
+                CreatePrimitiveMesh(heightColorArray, strideWidth, Vector3.Zero);
+                heightColorArray = new Color[0];
             }
 
-            public PrimitiveIndexedMesh(int subdivisionWidth, int subdividsionHeight, float scale, bool clockwise, bool invertNormals, bool directionalFaces)
+            public PrimitiveIndexedMesh(float[] heightArray, int strideWidth, Vector3 scale)
             {
-                CreatePrimitiveMesh(subdivisionWidth, subdividsionHeight, scale, clockwise, invertNormals, directionalFaces, 0);
+                heightColorArray = new Color[heightArray.Length];
+                for (int i = 0; i < heightArray.Length; i++)
+                {
+                    heightColorArray[i].R = GetAvgHeightFromFloatAsByte(heightArray[i]);
+                    heightColorArray[i].A = GetAvgHeightFromFloatAsByte(heightArray[i]);
+                }
+                CreatePrimitiveMesh(heightColorArray, strideWidth, scale);
+                heightColorArray = new Color[0];
             }
 
-            public PrimitiveIndexedMesh(float[] heightArray, int strideWidth, float scale, bool clockwise, bool invertNormals, bool directionalFaces)
+            public PrimitiveIndexedMesh(Texture2D heightTexture, Vector3 scale)
             {
-                CreatePrimitiveMesh(heightArray, strideWidth, scale, clockwise, invertNormals, directionalFaces, 0);
+                Color[] heightColorArray = new Color[heightTexture.Width * heightTexture.Height];
+                heightTexture.GetData<Color>(heightColorArray);
+                CreatePrimitiveMesh(heightColorArray, heightTexture.Width, scale);
+                heightColorArray = new Color[0];
             }
 
-            public void CreatePrimitiveMesh(float[] heightArray , int strideWidth, float scale, bool clockwise, bool invertNormals, bool directionalFaces, int faceIndex)
+            public void CreatePrimitiveMesh(Color[] heighColorArray, int strideWidth, Vector3 scale)
             {
                 List<VertexPositionNormalTexture> cubesFaceMeshLists = new List<VertexPositionNormalTexture>();
                 List<int> cubeFaceMeshIndexLists = new List<int>();
 
                 int subdivisionWidth = strideWidth;
-                int subdividsionHeight = (int)( heightArray.Length / strideWidth) ;
+                int subdividsionHeight = (int)(heighColorArray.Length / strideWidth);
 
                 if (subdivisionWidth < 2)
                     subdivisionWidth = 2;
@@ -337,81 +404,16 @@ namespace ShaderExamples
                     {
                         float stepU = (float)(x) / (float)(subdivisionWidth - 1);
 
-                        float hval =  heightArray[ GetIndex(x, y, strideWidth) ];
+                        float val = GetAvgHeightFromColorAsUnitLengthValue( heighColorArray[GetIndex(x, y, strideWidth) ] , AveragingOption);
+                        float hval = -val;
 
                         float X = Interpolate(left, right, stepU);
                         float Y = Interpolate(top, bottom, stepV);
 
-                        var p0 = new Vector3(stepU, stepV, hval) * scale ;
-                        var uv0 = new Vector2(stepU, stepV) ;
-                        var vert = GetVertice(p0, faceIndex, directionalFaces, depth, uv0) ;
-
-                        if (showOutput)
-                            System.Console.WriteLine("vert[" + vertCounter + "]: " + vert) ;
-
-                        cubesFaceMeshLists.Add(vert);
-                        vertCounter += 1;
-                    }
-                }
-
-                for (int y = 0; y < subdividsionHeight - 1; y++)
-                {
-                    for (int x = 0; x < subdivisionWidth - 1; x++)
-                    {
-                        var stride = subdivisionWidth;
-                        var faceVerticeOffset = stride * y + x;
-
-                        var tl = faceVerticeOffset;
-                        var bl = faceVerticeOffset + stride;
-                        var br = faceVerticeOffset + stride + 1;
-                        var tr = faceVerticeOffset + 1;
-
-                        AddQuadIndexes(0, tl, tr, bl, br, cubeFaceMeshIndexLists);
-
-                        if (showOutput)
-                            ConsoleOutput(0, tl, tr, bl, br, cubesFaceMeshLists);
-                    }
-                }
-                vertices = cubesFaceMeshLists.ToArray();
-                indices = cubeFaceMeshIndexLists.ToArray();
-            }
-
-            public void CreatePrimitiveMesh(int subdivisionWidth, int subdividsionHeight, float scale, bool clockwise, bool invertNormals, bool directionalFaces, int faceIndex)
-            {
-                List<VertexPositionNormalTexture> cubesFaceMeshLists = new List<VertexPositionNormalTexture>();
-                List<int> cubeFaceMeshIndexLists = new List<int>();
-
-                if (subdivisionWidth < 2)
-                    subdivisionWidth = 2;
-                if (subdividsionHeight < 2)
-                    subdividsionHeight = 2;
-
-                float depth = 0;
-
-                float left = -1f;
-                float right = +1f;
-                float top = -1f;
-                float bottom = +1f;
-
-                int vertCounter = 0;
-                for (int y = 0; y < subdividsionHeight; y++)
-                {
-                    float stepV = (float)(y) / (float)(subdividsionHeight - 1);
-                    for (int x = 0; x < subdivisionWidth; x++)
-                    {
-                        float stepU = (float)(x) / (float)(subdivisionWidth - 1);
-
-                        float X = Interpolate(left, right, stepU);
-                        float Y = Interpolate(top, bottom, stepV);
-
-                        var p0 = new Vector3(X, Y, depth) * scale;
+                        var p0 = new Vector3(stepU, stepV, hval) * scale;
                         var uv0 = new Vector2(stepU, stepV);
-                        var vert = GetVertice(p0, faceIndex, directionalFaces, depth, uv0);
 
-                        if (showOutput)
-                            System.Console.WriteLine("vert[" + vertCounter + "]: " + vert);
-
-                        cubesFaceMeshLists.Add(vert);
+                        cubesFaceMeshLists.Add(GetVertice(p0, uv0));
                         vertCounter += 1;
                     }
                 }
@@ -436,22 +438,6 @@ namespace ShaderExamples
                 }
                 vertices = cubesFaceMeshLists.ToArray();
                 indices = cubeFaceMeshIndexLists.ToArray();
-            }
-
-            public int GetIndex(int x, int y, int stride)
-            {
-                return x + y * stride;
-            }
-
-            public void GetIndexXy(int Index , int stride, out int x, out int y)
-            {
-                y = (int)(Index / stride);
-                x = Index - (y * stride);
-            }
-
-            private float Interpolate(float A, float B, float t)
-            {
-                return ((B - A) * t) + A;
             }
 
             public void AddQuadIndexes(int faceIndex, int tl, int tr, int bl, int br, List<int> cubeFaceMeshIndexLists)
@@ -463,6 +449,54 @@ namespace ShaderExamples
                     cubeFaceMeshIndexLists.Add(br);
                     cubeFaceMeshIndexLists.Add(tr);
                     cubeFaceMeshIndexLists.Add(tl);
+            }
+
+            public int GetIndex(int x, int y, int stride)
+            {
+                return x + y * stride;
+            }
+
+            public void GetIndexXy(int Index, int stride, out int x, out int y)
+            {
+                y = (int)(Index / stride);
+                x = Index - (y * stride);
+            }
+
+            private float Interpolate(float A, float B, float t)
+            {
+                return ((B - A) * t) + A;
+            }
+
+            private byte GetAvgHeightFromFloatAsByte(float v)
+            {
+                return (byte)(v * 255f);
+            }
+
+            private float GetAvgHeightFromColorAsUnitLengthValue(Color c, int option)
+            {
+            float result = 0;
+                switch (option)
+                {
+                    case AVERAGING_OPTION_USE_NONALPHACONSISTANTLY :
+                        result =  c.A > 0 ? 1f : 0f;
+                        break;
+                    case AVERAGING_OPTION_USE_HIGHEST:
+                        result = c.R;
+                        result = c.G > result ? c.G : result;
+                        result = c.B > result ? c.B : result;
+                        result = (c.R / 255f) * (c.A / 255f);
+                        break;
+                    case AVERAGING_OPTION_USE_AVERAGE:
+                        result = (((c.R + c.G + c.B) / 3f) * (c.A / 255f));
+                        break;
+                    case AVERAGING_OPTION_USE_RED:
+                        result = (c.R / 255f);
+                        break;
+                    default:
+                        result = (c.R / 255f);
+                        break;
+                }
+                return result;
             }
 
             public void ConsoleOutput(int faceIndex, int tl, int tr, int bl, int br, List<VertexPositionNormalTexture> cubesFaceMeshLists)
@@ -482,7 +516,7 @@ namespace ShaderExamples
             }
 
             // TODO we should generate smooth normals here if flat faces is false.
-            private VertexPositionNormalTexture GetVertice(Vector3 v, int faceIndex, bool directionalFaces, float depth, Vector2 uv)
+            private VertexPositionNormalTexture GetVertice(Vector3 v, Vector2 uv)
             {
                 return new VertexPositionNormalTexture(v, new Vector3(0, 0, 1) , uv);
             }
